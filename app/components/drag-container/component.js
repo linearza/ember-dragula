0477688d6fc9291e7711fa08cddc40a89b81f722
Michiel <linearza@users.noreply.github.com>
2016-04-04 16:49:02 +0200

    Added a basic dragula implementation with two groups and three items. Cleanup. Struggled to import dragula from npm and bower installations - ended up using the dist version

--- app/components/drag-container/component.js
+++ app/components/drag-container/component.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import dragula from 'dragula';
 
 const {
   Component, Logger: { info }
@@ -7,6 +8,10 @@ const {
 export default Component.extend({
 
   classNames: ['c_drag-container'],
+
+  drake: null,
+
+  dragulaContainerSelector: '.c_drag-group',
 
   pan() {
     info('drag-container: PAN');
@@ -21,6 +26,35 @@ export default Component.extend({
   press() {
     info('drag-container: PRESS');
     return false;
+  },
+
+  /*
+    http://haughtcodeworks.com/blog/software-development/enter-dragula/
+    http://haughtcodeworks.com/blog/software-development/bridging-dragula-and-ember/
+  */
+
+  willInsertElement() {
+    let drake = dragula(this.dragulaContainers(), this.dragulaOptions());
+    
+    drake.on("drop", (el, target, source, sibling) => {
+      info('drag-container: dropped');
+      this.sendAction("dropped", el, target, source, sibling);
+    });
+
+    this.set("drake", drake);
+  },
+
+  willDestroyElement() {
+    this.get("drake").destroy();
+    this.set("drake", null);
+  },
+
+  dragulaContainers() {
+    return this.$(this.dragulaContainerSelector).get();
+  },
+
+  dragulaOptions: () => {
+    return this.options || {};
   }
 
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,14 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+
+  app.import('vendor/dragula/dragula.js');
+  app.import('vendor/shims/dragula.js', {
+    exports: {
+      'dragula': ['default']
+    }
+  });
+
   return app.toTree();
+
 };
