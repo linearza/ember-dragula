14afa31d33fb8f15ab444bdfebe9afc5b995b4e4
Michiel <linearza@users.noreply.github.com>
2016-04-16 20:07:01 +0200

    Jshint fix

--- app/components/gestures-container/component.js
+++ app/components/gestures-container/component.js
@@ -2,7 +2,7 @@ import RecognizerMixin from 'ember-gestures/mixins/recognizers';
 import Ember from 'ember';
 
 const {
-  Component, Logger: { info }, inject: { service }
+  Component, Logger: { info }
 } = Ember;
 
 export default Component.extend(RecognizerMixin, {
@@ -22,5 +22,5 @@ export default Component.extend(RecognizerMixin, {
   press() {
     info('gestures-container: PRESS');
   }
-  
+
 });
